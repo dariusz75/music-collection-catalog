3b9c6d41337f9590c471429c4c9cc2044307e05d
Dariusz Franczak <dariusz@voly.co.uk>
2023-06-16 14:45:04 +0100

    env file implemented

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -1,14 +1,17 @@
 import { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 
+const DB_TOKEN = process.env.REACT_APP_MUSIC_COLLECTION_DB_TOKEN;
+
 const Collection: FC = () => {
 	const [albums, setAlbums] = useState([]);
 
 	useEffect(() => {
+    console.log('rendered', DB_TOKEN)
 		axios
 			.get('/albums', {
 				headers: {
-					Authorization: `Bearer patP9oVhHRJIDDO8W.3f8a5a428afdfde96abe26b05578db2aaf1eba80977e047b9c5e3c7cc3aa80b7`,
+					Authorization: `Bearer ${DB_TOKEN}`,
 				},
 			})
 			.then(function (response) {
