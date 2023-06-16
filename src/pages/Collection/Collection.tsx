71d6a4bcb22e3d9bc6db69e984b9901d97ce7c27
Dariusz Franczak <dariusz@voly.co.uk>
2023-06-16 16:40:34 +0100

    work in progress

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -4,24 +4,118 @@ import axios from 'axios';
 const DB_TOKEN = process.env.REACT_APP_MUSIC_COLLECTION_DB_TOKEN;
 
 const Collection: FC = () => {
+  const testData = [
+    {
+      id: "rec6jHXMmGsIkXKb8",
+      createdTime: "2023-06-12T20:42:28.000Z",
+      fields: {
+        Artist: "Nick Cave",
+        Title: "Murder Ballads",
+        Genre: [
+          "rec1WLEWT7bFVKyAk"
+        ],
+        Format: "CD",
+        PurchaseDate: "2003-11-11"
+      }
+    },
+    {
+      id: "rec6ycqLLe8LVIvJS",
+      createdTime: "2023-06-12T21:12:10.000Z",
+      fields: {
+        Artist: "Cypress Hill",
+        Title: "Till Death Do Us Part",
+        Genre: [
+          "recU45ztU9XMQGShJ"
+        ],
+        Format: "CD",
+        PurchaseDate: "2010-08-01"
+      }
+    },
+    {
+      id: "recDfJeV3tCLQDmx8",
+      createdTime: "2023-06-12T21:13:29.000Z",
+      fields: {
+        Artist: "Depeche Mode",
+        Title: "Violator",
+        Genre: [
+          "recKIbSdjSLbP3gk9"
+        ],
+        Format: "CD",
+        PurchaseDate: "2002-07-01"
+      }
+    },
+    {
+      id: "recLL4R1Gb48zwZv5",
+      createdTime: "2023-06-12T20:42:28.000Z",
+      fields: {
+        Artist: "Anette Askvik",
+        Title: "Liberty",
+        Genre: [
+          "reczPn3xYhy5U0Rup"
+        ],
+        Format: "CD",
+        PurchaseDate: "2022-11-01"
+      }
+    },
+    {
+      id: "recmtg3uEUmTybrqr",
+      createdTime: "2023-06-12T20:42:28.000Z",
+      fields: {
+        Artist: "Nick Cave",
+        Title: "Murder Ballads",
+        Genre: [
+          "rec1WLEWT7bFVKyAk"
+        ],
+        Format: "LP",
+        Purchase Date: "2022-07-01"
+      }
+    }
+  ];
+  
+  const [isLoading, setIsLoading] = useState(true);
 	const [albums, setAlbums] = useState([]);
+  const [error, setError] = useState({message:''});
+  
+  const getCollection = async () => {
+    try {
+      const res = await axios
+      .get('/albums', {
+        headers: {
+          Authorization: `Bearer ${DB_TOKEN}`,
+        },
+      });
+      const records = res?.data.records;
+      console.log('records are', records)
+      setAlbums(albums)
+      setIsLoading(false);
+    } catch(err) {
+        setError({message: error.message});
+    } finally {
+      setTimeout(()=>{console.log('albums are', albums)}, 500)
+      
+        
+    }
+    
+   
+  }
 
 	useEffect(() => {
     console.log('rendered', DB_TOKEN)
-		axios
-			.get('/albums', {
-				headers: {
-					Authorization: `Bearer ${DB_TOKEN}`,
-				},
-			})
-			.then(function (response) {
-				console.log(response);
-				setAlbums(response.data.records);
-				console.log('albums are', albums);
-			});
+		getCollection()
+    
 	}, []);
 
-	return <div>Collection Page Component</div>;
+
+
+  const listOfAlbums = <p>albums</p>
+  
+
+	return <div>
+    {isLoading && <p>Loading...</p>}
+    {!isLoading && error.message && <p>{error.message}</p>}
+    {!isLoading && !error.message && albums && listOfAlbums}
+    </div>;
 };
 
 export default Collection;
+
