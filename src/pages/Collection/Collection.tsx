a8f54864947bb1b12b95a98ee3f7b8bce5a589bb
Dariusz Franczak <dariusz@voly.co.uk>
2023-06-18 21:52:47 +0100

    work in progress

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -1,77 +1,109 @@
-import { FC, useEffect, useState } from 'react';
+import {  useEffect, useState } from 'react';
 import axios from 'axios';
 
 const DB_TOKEN = process.env.REACT_APP_MUSIC_COLLECTION_DB_TOKEN;
 
-const Collection: FC = () => {
-  const testData = [
-    {
-      id: "rec6jHXMmGsIkXKb8",
-      createdTime: "2023-06-12T20:42:28.000Z",
-      fields: {
-        Artist: "Nick Cave",
-        Title: "Murder Ballads",
-        Genre: [
-          "rec1WLEWT7bFVKyAk"
-        ],
-        Format: "CD",
-        PurchaseDate: "2003-11-11"
-      }
-    },
-    {
-      id: "rec6ycqLLe8LVIvJS",
-      createdTime: "2023-06-12T21:12:10.000Z",
-      fields: {
-        Artist: "Cypress Hill",
-        Title: "Till Death Do Us Part",
-        Genre: [
-          "recU45ztU9XMQGShJ"
-        ],
-        Format: "CD",
-        PurchaseDate: "2010-08-01"
-      }
-    },
-    {
-      id: "recDfJeV3tCLQDmx8",
-      createdTime: "2023-06-12T21:13:29.000Z",
-      fields: {
-        Artist: "Depeche Mode",
-        Title: "Violator",
-        Genre: [
-          "recKIbSdjSLbP3gk9"
-        ],
-        Format: "CD",
-        PurchaseDate: "2002-07-01"
-      }
-    },
-    {
-      id: "recLL4R1Gb48zwZv5",
-      createdTime: "2023-06-12T20:42:28.000Z",
-      fields: {
-        Artist: "Anette Askvik",
-        Title: "Liberty",
-        Genre: [
-          "reczPn3xYhy5U0Rup"
-        ],
-        Format: "CD",
-        PurchaseDate: "2022-11-01"
-      }
-    },
-    {
-      id: "recmtg3uEUmTybrqr",
-      createdTime: "2023-06-12T20:42:28.000Z",
-      fields: {
-        Artist: "Nick Cave",
-        Title: "Murder Ballads",
-        Genre: [
-          "rec1WLEWT7bFVKyAk"
-        ],
-        Format: "LP",
-        Purchase Date: "2022-07-01"
-      }
+const testData: albumsDataType = {
+  records:[
+  {
+    id: "rec6jHXMmGsIkXKb8",
+    createdTime: "2023-06-12T20:42:28.000Z",
+    fields: {
+      Artist: "Nick Cave",
+      Title: "Murder Ballads",
+      Genre: [
+        "rec1WLEWT7bFVKyAk"
+      ],
+      Format: "CD",
+      PurchaseDate: "2003-11-11"
     }
-  ];
-  
+  },
+  {
+    id: "rec6ycqLLe8LVIvJS",
+    createdTime: "2023-06-12T21:12:10.000Z",
+    fields: {
+      Artist: "Cypress Hill",
+      Title: "Till Death Do Us Part",
+      Genre: [
+        "recU45ztU9XMQGShJ"
+      ],
+      Format: "CD",
+      PurchaseDate: "2010-08-01"
+    }
+  },
+  {
+    id: "recDfJeV3tCLQDmx8",
+    createdTime: "2023-06-12T21:13:29.000Z",
+    fields: {
+      Artist: "Depeche Mode",
+      Title: "Violator",
+      Genre: [
+        "recKIbSdjSLbP3gk9"
+      ],
+      Format: "CD",
+      PurchaseDate: "2002-07-01"
+    }
+  },
+  {
+    id: "recLL4R1Gb48zwZv5",
+    createdTime: "2023-06-12T20:42:28.000Z",
+    fields: {
+      Artist: "Anette Askvik",
+      Title: "Liberty",
+      Genre: [
+        "reczPn3xYhy5U0Rup"
+      ],
+      Format: "CD",
+      PurchaseDate: "2022-11-01"
+    }
+  },
+  {
+    id: "recmtg3uEUmTybrqr",
+    createdTime: "2023-06-12T20:42:28.000Z",
+    fields: {
+      Artist: "Nick Cave",
+      Title: "Murder Ballads",
+      Genre: [
+        "rec1WLEWT7bFVKyAk"
+      ],
+      Format: "LP",
+      PurchaseDate: "2022-07-01"
+    }
+  }
+]};
+interface AlbumListProps {
+  recordData: albumsDataType;
+};
+
+type RecordType = {
+  id: string;
+  createdTime: string;
+  fields: {
+    Artist: string;
+    Title: string;
+    Genre: string[];
+    Format: string;
+    PurchaseDate: string;
+  }
+};
+
+type albumsDataType = {
+  records: RecordType[];
+};
+
+const AlbumList = (props: AlbumListProps) => {
+  const { records  } = props.recordData;
+return (
+  <>
+  {records.map((record) => {
+    return <p key={record.id}>{record.fields.Artist}</p>
+  })}
+  </> 
+  )
+}
+
+
+const Collection = () => {
   const [isLoading, setIsLoading] = useState(true);
 	const [albums, setAlbums] = useState([]);
   const [error, setError] = useState({message:''});
@@ -84,19 +116,16 @@ const Collection: FC = () => {
           Authorization: `Bearer ${DB_TOKEN}`,
         },
       });
-      const records = res?.data.records;
-      console.log('records are', records)
-      setAlbums(albums)
+      const records = res?.data;
+      console.log('response is', res);
+      setAlbums(records);
+      console.log('records from api are', records);
       setIsLoading(false);
     } catch(err) {
         setError({message: error.message});
     } finally {
-      setTimeout(()=>{console.log('albums are', albums)}, 500)
-      
-        
+      setIsLoading(false);
     }
-    
-   
   }
 
 	useEffect(() => {
@@ -107,13 +136,15 @@ const Collection: FC = () => {
 
 
 
-  const listOfAlbums = <p>albums</p>
+// return <div>
+//   <AlbumList recordData={testData} />
+// </div>
   
 
 	return <div>
     {isLoading && <p>Loading...</p>}
     {!isLoading && error.message && <p>{error.message}</p>}
-    {!isLoading && !error.message && albums && listOfAlbums}
+    {!isLoading && !error.message && albums && <AlbumList recordData={testData} />}
     </div>;
 };
 
